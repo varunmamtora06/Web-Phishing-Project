884a432cdce6935647a33eb9c58e57e7e4328969
Varun Mamtora <varunmamtora@gmail.com>
2021-05-27 18:43:22 +0530

    update settings

--- WebPhishing/settings.py
+++ WebPhishing/settings.py
@@ -123,10 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [BASE_DIR/ 'static']
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR/ 'staticfiles'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
